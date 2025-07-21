df518b7a65c546a7865aef03b526af0dac4a7965
misaka10987 <misaka10987@outlook.com>
2025-07-21 20:10:52 +0800

    implement twin prime searching & plotting

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+#!/usr/bin/uv run
+
+# pyright: reportUnknownMemberType=false, reportUnknownVariableType=false, reportMissingImports=false
+
+from sympy import isprime
+import matplotlib.pyplot as plt
+
+SEARCH_MAX = 1000
+
+
+def check(n: int) -> bool:
+    return isprime(n) and isprime(n + 2)
+
+
 def main():
-    print("Hello from twin-prime!")
+    print(f'Searching for twin primes under {SEARCH_MAX}')
+    found = [i for i in range(SEARCH_MAX) if check(i)]
+    print(f'Found twin primes: {found}')
+    plt.hist(found, bins=10)
+    plt.title('Distrubution of Twin Primes')
+    plt.xlabel('The smaller twin')
+    plt.ylabel('Count')
+    plt.show()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
